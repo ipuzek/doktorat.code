adc4b3b6df573008e0378e0346675fa3d7685fe6
Ivan Puzek <ivan.puzek@gmail.com>
2017-06-21 22:58:18 +0200

    niš ovdje nisam zmodeliro

--- 02_explore.R
+++ 02_explore.R
@@ -1,6 +1,8 @@
 ### SVEsplit doktorat ### EDA ###
 
-# EXPLORE
+
+# explore via vizz --------------------------------------------------------
+
 ggplot(sveST, aes(x = to_factor(imovina.kucanstva), y = prihod.PC)) +
   geom_boxplot() + 
   geom_jitter(alpha = .2) + ylim(c(0,10000))
@@ -17,3 +19,28 @@ ggplot(sveST, aes(x = dmg2, y = prihod.PC, colour = to_factor(dmg1))) +
 
 # imovina vs prihod vs dob!
 
+
+# explore via modelling -----------------------------------------------
+
+library(broom)
+library(arm)
+
+tidy = koeficijenti
+augment = vrijednosti (ala mutate)
+glance = pojedina vrijednost (ala R2)
+
+
+# prihod _ imovina --------------------------------------------------------
+
+aov(prihod.PC ~ imovina.kucanstva, data = sveST) %>% tidy
+
+
+# prihod _ dob _ spol -----------------------------------------------------
+
+lm(prihod.PC ~ dmg1 + dmg2, data = sveST) %>% display()
+lm(prihod.PC ~ spol + dmg2, data = sveST) %>% display()
+
+lm(prihod.PC ~ dmg1 + dmg2, data = sveST, weights = rim_w_1) %>% display()
+lm(prihod.PC ~ spol + dmg2, data = sveST, weights = rim_w_1) %>% display()
+
+# imovina vs prihod vs dob! TODO
