8b0c664d14fc933841938900bf3d29f9f0fe3d74
Ivan Puzek <ivan.puzek@gmail.com>
2017-07-08 11:11:40 +0200

    prediktori

--- 01_1__dodatni_prediktori.R
+++ 01_1__dodatni_prediktori.R
@@ -38,3 +38,36 @@ sveST$rel2_r <- sveST$rel2 %>%
              NZBO = "Ne zna - bez odgovora") %>% 
   na_if("NZBO")  #  %>% fct_count()
 
+
+# sockap ------------------------------------------------------------------
+
+tmp.sockap.df <- sveST %>% select(sk1_1 : sk1_7) %>% 
+  mutate_all(isnt_na) %>% 
+  transmute(sockap.veza = sk1_1 + sk1_2 + sk1_3 + sk1_4 + sk1_5 + sk1_6 + sk1_7)
+
+sveST$sockap.veza <- factor(tmp.sockap.df$sockap.veza) %>% 
+  fct_collapse(`Nema vezu nigdje` = "0",
+               `Ima vezu` = c("1", "2", "3", "4", "5", "6", "7"))
+
+rm(tmp.sockap.df)
+
+
+# politika ----------------------------------------------------------------
+
+tmp.politika.df <-
+  sveST %>% 
+  select(starts_with("pp1x"))
+
+pol_recode <- function(x) {
+  recode(x,
+         `1` = 2, `2` = 1,
+         `3` = 0, `98` = 0, `99` = 0)
+}
+
+tmp.politika.df.2 <- 
+  map_df(tmp.politika.df, as.numeric) %>% 
+  map_df(pol_recode)
+
+sveST$nef.politika <- rowSums(tmp.politika.df.2)
+
+rm(tmp.politika.df, tmp.politika.df.2)
